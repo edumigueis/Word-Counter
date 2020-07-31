8c7ef83d5e548c4bec6457cf420ff48d5259cb4a
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-07-31 11:26:04 -0300

    Proper Counter Implementation

---
+++ js/word-counter.js
@@ -0,0 +1,29 @@
+var countTarget = document.querySelector("#input-text");
+var wordCount = document.querySelector("#word-count");
+var characterCount = document.querySelector("#character-count");
+
+var count = function () {
+  var characters = countTarget.value;
+  var characterLength = characters.length;
+
+  var words = characters.split(/[\n\r\s]+/g).filter(function (word) {
+    return word.length > 0;
+  });
+
+  wordCount.innerHTML = words.length;
+  characterCount.innerHTML = characterLength;
+};
+
+countTarget.textContent = "Almost before we knew it, we had left the ground."
+
+count();
+
+window.addEventListener(
+  "input",
+  function (event) {
+    if (event.target.matches("#input-text")) {
+      count();
+    }
+  },
+  false
+);
